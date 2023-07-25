5de9062bbfa03678e947976b5d04a0532b350777
Mark Tyneway <mark.tyneway@gmail.com>
2023-07-24 21:56:28 -0700

    fixup

--- op-chain-ops/genesis/genesis.go
+++ op-chain-ops/genesis/genesis.go
@@ -11,6 +11,7 @@ import (
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/core"
 	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/params"
 )
 
